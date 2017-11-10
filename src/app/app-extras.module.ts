d088ba8113605c18fe62314c36753dddf55a0f0a
FireBat <FireBat>
2017-11-09 21:33:48 -0500

    unwired avanced demo

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -1,24 +1,14 @@
 import { NgModule } from '@angular/core';
 
-import { SkyAppConfig } from '@blackbaud/skyux-builder/runtime';
 import { JTagInputModule } from './public/src/modules/components/j-tag-input/j-tag-input.module';
-import { JTagDemoModule } from './public/src/modules/components/j-tag-demo';
-import { JTagDemoService } from './public/src/modules/components/j-tag-demo';
 
 // Specify entry components, module-level providers, etc. here.
 @NgModule({
-  imports: [
-    JTagDemoModule
-  ],
+  imports: [JTagInputModule],
   exports: [
     JTagInputModule
   ],
   providers: [
-    // The config service acts as an adaptor for skyuxconfig.json:
-    {
-      provide: JTagDemoService,
-      useExisting: SkyAppConfig
-    }
   ],
   entryComponents: []
 })
